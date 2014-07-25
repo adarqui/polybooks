2a51904acee3bbb185b548d73bdc047d7332f123
Andrew Darqui <andrew.darqui@gmail.com>
2014-07-25 02:13:48 +0000

    purescript

---
+++ cookbooks/lang_fay/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: lang_fay
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute 'cabal install -g fay'
---
+++ cookbooks/lang_purescript/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: lang_purescript
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute 'cabal install -g purescript'
